a8c1deb55ff14c6a501f6650e39e8899bb8a4011
David A Roberts <d@vidr.cc>
2024-03-21 18:24:10 +1000

    inlining

--- lab001/hdmi.py
+++ lab001/hdmi.py
@@ -3,12 +3,14 @@
 # python3 -m litex_boards.targets.colorlight_i5 --board i9 --revision 7.2 --cpu-type None \
 #   --with-video-colorbars --video-timing 1024x768@60Hz --build --load --ecppack-compress
 
+from migen import *
+
 from litex_boards.platforms import colorlight_i5
 from litex_boards.targets.colorlight_i5 import _CRG
 
 from litex.soc.integration.soc_core import SoCCore
 from litex.soc.integration.builder import Builder
-from litex.soc.cores.video import VideoHDMIPHY
+from litex.soc.cores.video import VideoHDMIPHY, VideoTimingGenerator, ColorBarsPattern
 from litex.soc.cores.video import video_timings
 
 class BaseSoC(SoCCore):
@@ -25,7 +27,21 @@ class BaseSoC(SoCCore):
         SoCCore.__init__(self, platform, int(sys_clk_freq), ident="LiteX SoC on Colorlight i9", cpu_type=None)
 
         self.videophy = VideoHDMIPHY(platform.request("gpdi"), clock_domain="hdmi")
-        self.add_video_colorbars(phy=self.videophy, timings=video_timing, clock_domain="hdmi")
+
+        # Video Timing Generator.
+        video_colorbars_vtg = VideoTimingGenerator(default_video_timings=video_timing)
+        video_colorbars_vtg = ClockDomainsRenamer("hdmi")(video_colorbars_vtg)
+        self.add_module("video_colorbars_vtg", video_colorbars_vtg)
+
+        # ColorsBars Pattern.
+        video_colorbars = ClockDomainsRenamer("hdmi")(ColorBarsPattern())
+        self.add_module("video_colorbars", video_colorbars)
+
+        # Connect Video Timing Generator to ColorsBars Pattern.
+        self.comb += [
+            video_colorbars_vtg.source.connect(video_colorbars.vtg_sink),
+            video_colorbars.source.connect(self.videophy.sink)
+        ]
 
 def main():
     soc = BaseSoC()
